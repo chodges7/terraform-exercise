1d4eef10c9536fbbc9adac130b34b4729351501b
chodges7 <chodges7@mail.csuchico.edu>
2021-12-03 12:31:53 -0800

    chore: auto-indent code

--- exercise-1/my-instance.tf
+++ exercise-1/my-instance.tf
@@ -1,60 +1,60 @@
 provider "aws" {
     region = "us-west-1"
-    default_tags {
-        tags = {
-            Owner   = "Christian",
-            Client  = "Internal",
-            Project = "Bootcamp"
+        default_tags {
+            tags = {
+                Owner   = "Christian",
+                Client  = "Internal",
+                Project = "Bootcamp"
+            }
         }
-    }
 }
 
 resource "aws_security_group" "christian-bootcamp" {
-  name        = "christian-bootcamp"
-  description = "Created by Terraform in exercise 1 of bootcamp"
+    name        = "christian-bootcamp"
+        description = "Created by Terraform in exercise 1 of bootcamp"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["98.208.42.143/32"]
-  }
+        ingress {
+            from_port   = 22
+                to_port     = 22
+                protocol    = "tcp"
+                cidr_blocks = ["98.208.42.143/32"]
+        }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  tags = {
-    Name = "christian-bootcamp",
-  }
+    tags = {
+        Name = "christian-bootcamp",
+    }
 }
 
 resource "aws_security_group" "christian-http" {
     name        = "christian-http"
-    description = "Created by Terraform in exercise 1 of bootcamp"
+        description = "Created by Terraform in exercise 1 of bootcamp"
 
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["98.208.42.143/32"]
-    }
+        ingress {
+            from_port   = 80
+                to_port     = 80
+                protocol    = "tcp"
+                cidr_blocks = ["98.208.42.143/32"]
+        }
 
     ingress {
         from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["98.208.42.143/32"]
+            to_port     = 443
+            protocol    = "tcp"
+            cidr_blocks = ["98.208.42.143/32"]
     }
 
     egress {
         from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
     }
 
     tags = {
@@ -68,12 +68,12 @@ variable "aws_instance_type" {
 
 resource "aws_instance" "christian-terraform" {
     ami                     = "ami-054965c6cd7c6e462"
-    instance_type           = var.aws_instance_type
-    key_name                = "christian-keypair"
-    vpc_security_group_ids  = [
+        instance_type           = var.aws_instance_type
+        key_name                = "christian-keypair"
+        vpc_security_group_ids  = [
         aws_security_group.christian-bootcamp.id,
         aws_security_group.christian-http.id]
-    tags = {
-        Name = "christian-terraform-instance",
-    }
+            tags = {
+                Name = "christian-terraform-instance",
+            }
 }
