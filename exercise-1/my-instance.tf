0b98ff7afb68349fb8b87ac273d5ef74988c17f1
chodges7 <chodges7@mail.csuchico.edu>
2021-12-02 18:28:01 -0800

    feat: add a second security group to instance

--- exercise-1/my-instance.tf
+++ exercise-1/my-instance.tf
@@ -21,15 +21,44 @@ resource "aws_security_group" "christian-bootcamp" {
   }
 
   tags = {
-    Name = "christian-bootcamp"
+    Name = "christian-bootcamp",
+    Owner = "christian",
+    Client = "Internal",
   }
+}
+
+resource "aws_security_group" "christian-http" {
+    name        = "christian-http"
+    description = "Created by Terraform in exercise 1 of bootcamp"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["132.241.174.76/32"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "christian-bootcamp",
+        Owner = "christian",
+        Client = "Internal",
+    }
 }
 
 resource "aws_instance" "christian-terraform" {
     ami                     = "ami-054965c6cd7c6e462"
     instance_type           = "t2.micro"
     key_name                = "christian-keypair"
-    vpc_security_group_ids  = [aws_security_group.christian-bootcamp.id]
+    vpc_security_group_ids  = [
+        aws_security_group.christian-bootcamp.id,
+        aws_security_group.christian-http.id]
     tags = {
         Name = "christian-terraform-instance",
         Client = "Internal",
